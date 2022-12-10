1a1b4df72526148ede3d3dfd12dc62dbff22a874
Parkhicks <hic21003@byui.edu>
2022-12-10 14:23:58 -0700

    final project

--- Final/Takeoff.py
+++ Final/Takeoff.py
@@ -1,22 +1,24 @@
 import csv
 import math
 print()
-"""
+
 def main():
-    processed = "start"
+    number=1
     print("Welcome to the Automated Construction Take-off Calculator, this program is designed with a couple options of getting data\nyou can either reference different documents with the quantities prewritten into them. These can be either .txt or .csv files.")
     print("Second, we can go through all the phases of the construction process and ask you to enter the quantities manually. After retrieving\nthe quantities either by reference or input we'll go through it again and ask for the price, or ask for a file with prices.")
-    while processed != "file" or processed != "manual":
+    while number == 1:
         quantities = input("To begin do you have a .txt or .csv file with the written product and the quantitiy of them? or would you like to enter \nthem manually? type either 'file' or 'manual' ")
         processed = quantities.strip()
         processed = processed.lower()
-        if processed != "file" or processed != "manual":
-            print("Invalid input, please enter either 'file' or 'manual' ")
-        elif processed =="file":
+        if processed == "file":
+            number=0
             files_q()
         elif processed =="manual":
+            number =0
             inputted_q()
-"""
+        else:
+            print("Invalid input, please enter either 'file' or 'manual' ")
+
 
 #creating empty dictionaries for all the values that program will return to the user stored so they can be written to a file later on 
 foundation_dict ={}
@@ -25,21 +27,29 @@ flooring_dict={}
 fixtures_dict={}
 roofing_dict={}
 
+#Setting total price
+total=0
 
-    
+#function to compute the cost of different elements within a function
+def price_out(quantity,cost):
+    item_cost = quantity*cost
+    global total
+    total = total + item_cost
+#function branch with all the functions to get the quantities and prices from the user. 
 def inputted_q():
     #the function that is going to manually ask them questions to get quantities
     print("\nSounds good, we'll go through asking you questions to get quantities.")
     foundation()
     price_foundation()
-    #flooring()
-    #price_flooring()
+    flooring()
+    price_flooring()
     walls()
     price_walls()
     fixtures()
     price_fixtures()
     roofing()
     price_roofing()
+    print("\nThe total for the entire project is: $"+ str(total))
 
 # all functions defined to get the required quantities and prices from the user
 
@@ -76,22 +86,62 @@ def price_foundation():
     mud_sill_cost = float(input("What is the price of a single 2'x4'x12''? "))
     anchor_cost = float(input("What is the price of one anchor bolt? "))
     quantity = foundation_dict.get("footing yardage")
+    price_out(quantity,conc_cost)
     foundation_dict["footing yardage"] =[quantity,conc_cost]
     quantity = foundation_dict.get("foundation yardage")
+    price_out(quantity,conc_cost)
     foundation_dict["foundation yardage"] =[quantity,conc_cost]
     quantity = foundation_dict.get("mud sill")
+    price_out(quantity,mud_sill_cost)
     foundation_dict["mud sill"] =[quantity,mud_sill_cost]
     quantity = foundation_dict.get("anchor bolts")
+    price_out(quantity,anchor_cost)
     foundation_dict["anchor bolts"] =[quantity,anchor_cost]
-    
 
+    
 #flooring and cost of flooring
-#def flooring():
-#def price_flooring():
+def flooring():
+    print("\nNext we'll ask questions about the flooring. ")
+    rim_board= float(input("What is the perimeter of the floor area? (in feet) "))
+    length = float(input("What is the length of the building in feet? "))
+    area = float(input("What is the area of the floor? "))
+    sheets = math.ceil(area/32)
+    cont = "false"
+    while cont == "false" :
+        spacing = int(input("What is the spacing for the floor joists? 16' or 24' OC? (16 or 24)" ))
+        if spacing == 16:
+            cont ="true"
+        elif spacing == 24:
+            cont ="true"
+        else:
+            print("Please input either 16 or 24.")
+            cont="false"
+    joists = (length*12 /spacing)
+    boards = math.ceil(rim_board/12)
+    print("\nYou'll need "+ str(boards) + " 2' x 10' x 12'' boards for the rim boards")
+    print("You'll need " + str(joists+1) + " premanufactured joists for the floor. ")
+    print("You'll need "+ str(sheets) + " of standard 4x8 sheets of plywood ")
+    flooring_dict["joists"] =joists+1
+    flooring_dict["rimboards"] =boards
+    flooring_dict["sheeting"] = sheets
+def price_flooring():
+    joist_cost = float(input("What is the cost of a premanufactured truss? "))
+    board_cost = float(input("What is the price of a 2' x 10' x 12'' board? "))
+    sheet_cost = float(input("What is the price of a 4x8 sheet of plywood? "))
+    quantity = flooring_dict.get("joists")
+    price_out(quantity,joist_cost)
+    flooring_dict["joists"] = [quantity,joist_cost]
+    quantity = flooring_dict.get("rimboards")
+    price_out(quantity,board_cost)
+    flooring_dict["rimboards"] = [quantity,board_cost]
+    quantity = flooring_dict.get("sheeting")
+    price_out(quantity,sheet_cost)
+    flooring_dict["sheeting"] = [quantity,sheet_cost]
 
 
 #walls and cost of walls
 def walls():
+    print("\nNow we'll do the walls")
     l_feet_ext = int(input("What is the total linear feet of all the exterior walls? "))
     l_feet_int = int(input("What is the total linear feet of all the interior walls? "))
     w_h = int(input("What is the height of the ceilings? (in feet) "))
@@ -122,19 +172,25 @@ def price_walls():
     ext_cost = float(input("What is the price of a 1/2'' thick OSB plywood? "))
     int_cost = float(input("What is the price of a 5/8'' thick gypsum board? "))
     quantity = walls_dict.get("plates")
+    price_out(quantity,plate_cost)
     walls_dict["plates"] =[quantity,plate_cost]
     quantity = walls_dict.get("studs")
+    price_out(quantity,stud_cost)
     walls_dict["studs"] =[quantity,stud_cost]
     quantity = walls_dict.get("insulation")
+    price_out(quantity,insulation_cost)
     walls_dict["insulation"] =[quantity,insulation_cost]
     quantity = walls_dict.get("exterior sheeting")
+    price_out(quantity,ext_cost)
     walls_dict["exterior sheeting"] =[quantity,ext_cost]
     quantity = walls_dict.get("interior sheeting")
+    price_out(quantity,int_cost)
     walls_dict["interior sheeting"] =[quantity,int_cost]
 
 
 #fixtures and cost of fixtures
 def fixtures():
+    print("\nNow we'll do the fixtures. ")
     doors = input("How many doors are you going to have? (both interior & exterior) ")
     windows =input("How many windows are you going to have? ")
     fixtures_dict["doors"]= doors
@@ -143,13 +199,16 @@ def price_fixtures():
     door_cost = float(input("What is the cost of a door? "))
     win_cost = float(input("What is the cost of a window? "))
     quantity = fixtures_dict.get("doors")
+    price_out(quantity,door_cost)
     fixtures_dict["doors"] = [quantity,door_cost]
     quantity = fixtures_dict.get("windows")
+    price_out(quantity,win_cost)
     fixtures_dict["windows"] = [quantity,win_cost]
 
 
 #roofing and cost of roofing
 def roofing():
+    print("\nNow we'll do the roofing. ")
     have = input("Do you know the surface area of the roof? (Y or N) ")
     roof_length = input("What is the length of the area you are going to have a roof over? (in feet) ")
     roof_width = input("What is the width of the area you are going to have a roof over? (in feet) ")
@@ -177,8 +236,10 @@ def price_roofing():
     truss_cost = float(input("What is the cost of a truss? "))
     square_shingle_cost = float(input("What is the cost of a square of shingles? "))
     quantity = roofing_dict.get("trusses")
+    price_out(quantity,truss_cost)
     roofing_dict["trusses"] = [quantity,truss_cost]
     quantity = roofing_dict.get("shingle squares")
+    price_out(quantity,square_shingle_cost)
     fixtures_dict["shingle squares"] = [quantity,square_shingle_cost]
 
 
@@ -190,3 +251,5 @@ def files_q():
 #foundation()
 #price_foundation()
 #print(foundation_dict)
+
+main()
